43d2162cfcda8870a1f5fdd4efa83e552173dfdb
Nour-Nd <silsila.tv@gmail.com>
2023-07-14 13:47:21 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- The entry point
+ *
+ * Return: 0 (The program succeeded)
+ */
+
+int main(void)
+
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+for (c = 'A'; c <= 'Z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}

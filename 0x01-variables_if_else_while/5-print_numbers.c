be71f03dc99c91df47f6c1c4db9ee5497298f1de
Nour-Nd <silsila.tv@gmail.com>
2023-07-14 14:16:49 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- The entry point
+ *
+ * Return: 0 (The program succeeded)
+ */
+
+int main(void)
+
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
+}

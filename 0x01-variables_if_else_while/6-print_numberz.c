1ec6d3cc65dc8ff417ef8cc06898587e80513e04
Nour-Nd <silsila.tv@gmail.com>
2023-07-14 14:24:49 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- The entry point
+ *
+ * Return: 0 (The program succeeded)
+ */
+
+int main(void)
+
+{
+int k;
+for (k = '0'; k <= '9'; k++)
+{
+	putchar(k);
+}
+putchar('\n');
+return (0);
+}
